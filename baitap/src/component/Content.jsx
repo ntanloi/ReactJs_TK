296744b6f61463330f3fc5d8955a478061382281
ntanloi <nguyenloidt.052017@gmail.com>
2025-04-08 13:22:32 +0700

    4. DataTable + get API

--- baitap/src/component/Content.jsx
+++ baitap/src/component/Content.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react"
+import avt from '/img/avatar.png'
 
 export default function Content() {
 
     const [overview, setOverview] = useState([])
+    const [tableData, setTableData] = useState([])
 
     useEffect(() => {
         fetch('http://localhost:3000/overview')
@@ -15,6 +17,19 @@ export default function Content() {
             .then((data) => setOverview(data))
             .catch((err) => console.error("Fetch error:", err));
     }, []);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/table')
+            .then((res) => {
+                if (!res.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                return res.json();
+            })
+            .then((data) => setTableData(data))
+            .catch((err) => console.error("Fetch error:", err));
+    }, [])
+
     
 
     return ( 
@@ -67,60 +82,27 @@ export default function Content() {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td><input type="checkbox" /></td>
-                        <td>Elizabeth Lee</td>
-                        <td>AvatarSystems</td>
-                        <td>$559</td>
-                        <td>10/07/2023</td>
-                        <td><span className="status-new">New</span></td>
-                        <td><button>✏️</button></td>
-                    </tr>
-                    <tr>
-                        <td><input type="checkbox" /></td>
-                        <td>Carlos Garcia</td>
-                        <td>SmoozeShift</td>
-                        <td>$747</td>
-                        <td>24/07/2023</td>
-                        <td><span className="status-new">New</span></td>
-                        <td><button>✏️</button></td>
-                    </tr>
-                    <tr>
-                        <td><input type="checkbox" /></td>
-                        <td>Elizabeth Bailey</td>
-                        <td>Prime Time Telecome</td>
-                        <td>$564</td>
-                        <td>08/08/2023</td>
-                        <td><span className="status-progress">In-progress</span></td>
-                        <td><button>✏️</button></td>
-                    </tr>
-                    <tr>
-                        <td><input type="checkbox" /></td>
-                        <td>Ryan Brown</td>
-                        <td>OmniTech Corporation</td>
-                        <td>$541</td>
-                        <td>31/08/2023</td>
-                        <td><span className="status-progress">In-progress</span></td>
-                        <td><button>✏️</button></td>
-                    </tr>
-                    <tr>
-                        <td><input type="checkbox" /></td>
-                        <td>Ryan Young</td>
-                        <td>DataStream Inc.</td>
-                        <td>$769</td>
-                        <td>01/05/2023</td>
-                        <td><span className="status-completed">Completed</span></td>
-                        <td><button>✏️</button></td>
-                    </tr>
-                    <tr>
-                        <td><input type="checkbox" /></td>
-                        <td>Hailey Adam</td>
-                        <td>FlowRush</td>
-                        <td>$922</td>
-                        <td>10/06/2023</td>
-                        <td><span className="status-completed">Completed</span></td>
-                        <td><button>✏️</button></td>
-                    </tr>
+                    {
+                        tableData.map((item) => {
+                            return (
+                                <tr key={item.id}>
+                                    <td><input type="checkbox" /></td>
+                                    <td className="td-name"><img src={item.img} alt="" />{item.name}</td>
+                                    <td>{item.company}</td>
+                                    <td>{item.orderValue}</td>
+                                    <td>{item.orderDate}</td>
+                                    <td><span className={
+                                        item.status === "New" ? 'status-new' :
+                                        item.status === "In-progress" ? 'status-progress' :
+                                        item.status === "Completed" ? 'status-completed' : ''}>
+                                        {item.status}
+                                    </span>
+                                    </td>
+                                    <td><button>✏️</button></td>
+                                </tr>
+                            )
+                        })
+                    }
                 </tbody>
             </table>
         </div>
